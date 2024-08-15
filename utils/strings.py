a88e0ba7d6c59b637978ee5e19f8fc31ac8f3000
DanielSakhnovich <daniels080593@gmail.com>
2024-08-15 10:49:14 +0300

    strings type condition

--- utils/strings.py
+++ utils/strings.py
@@ -1,37 +1,49 @@
 
-def REPLACE(container: str, find: str, replace: str) -> str:
-    containerSize = len(container)
-    findSize = len(find)
-    replaceSize = len(replace)
-    i = 0
-    for _ in range(containerSize):
-        if i > containerSize - findSize:
-            break
-        text = container[i:i + findSize]
-        if text == find:
-            preText = container[:i]
-            postText = container[i + findSize:]
-            container = preText + replace + postText
-            containerSize = len(container)
-            i = i + replaceSize
-        else:
-            i = i + 1
+def REPLACE(container, find, replace) -> str:
+    if not (type(container) == str) or not (type(container) == str) or not (type(replace) == str):
+        raise TypeError
+    else:
+        containerSize = len(container)
+        findSize = len(find)
+        replaceSize = len(replace)
+        i = 0
+        for _ in range(containerSize):
+            if i > containerSize - findSize:
+                break
+            text = container[i:i + findSize]
+            if text == find:
+                preText = container[:i]
+                postText = container[i + findSize:]
+                container = preText + replace + postText
+                containerSize = len(container)
+                i = i + replaceSize
+            else:
+                i = i + 1
     return container
 
 
-def isUpper(text: str) -> bool:
-    for c in text:
-        if ord(c) < 65 or ord(c) > 90:
-            return False
-    return True
+def isUpper(text) -> bool:
+    if not type(text) == str:
+        raise TypeError
+    else:
+        for c in text:
+            if ord(c) < 65 or ord(c) > 90:
+                return False
+        return True
 
 
-def isLower(text: str) -> bool:
-    for c in text:
-        if ord(c) < 32 + 65 or ord(c) > 32 + 90:
-            return False
-    return True
+def isLower(text) -> bool:
+    if not type(text) == str:
+        raise TypeError
+    else:
+        for c in text:
+            if ord(c) < 32 + 65 or ord(c) > 32 + 90:
+                return False
+        return True
 
 
-def CONCAT(first: str, last: str) -> str:
+def CONCAT(first, last) -> str:
+    if not (type(first) == str) or not (type(last) == str):
+        raise TypeError
     return first + last
+
